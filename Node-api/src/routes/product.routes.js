a07485f7c560bf4273a3c72b2e58b0f84b624434
sakshigithubssuk <165806049+sakshigithubssuk@users.noreply.github.com>
2025-06-28 18:57:19 +0530

    Update product.routes.js

--- Node-api/src/routes/product.routes.js
+++ Node-api/src/routes/product.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const productController= require('../controller/productcontroller.js');
 const authenticate = require('../middlware/authenticate.js');
 
-router.get("/",authenticate,productController.getAllProducts);
-router.get("/id/:id",authenticate,productController.findProductById);
+router.get("/",productController.getAllProducts);
+router.get("/id/:id",productController.findProductById);
 
 module.exports=router;
