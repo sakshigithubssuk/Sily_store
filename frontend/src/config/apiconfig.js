e2ee4d879547af6a6e72e366477e0dd273a639c6
sakshigithubssuk <165806049+sakshigithubssuk@users.noreply.github.com>
2025-06-28 17:50:17 +0530

    Update apiconfig.js

--- frontend/src/config/apiconfig.js
+++ frontend/src/config/apiconfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const API_BASE_URL="http://localhost:5454";
+export const API_BASE_URL="https://sily-store-backend.onrender.com";
 const jwt = localStorage.getItem("jwt");
 export  const api = axios.create({
   baseURL:API_BASE_URL,
