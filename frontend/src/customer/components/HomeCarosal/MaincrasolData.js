d15884a14dedea2a9564975f31d056391bcf050c
sakshigithubssuk <sakshissuk35@gmail.com>
2025-06-28 15:43:07 +0530

    Fixed submodule issue and added actual frontend files

---
+++ frontend/src/customer/components/HomeCarosal/MaincrasolData.js
@@ -0,0 +1,14 @@
+import frontpage1 from "../../../assets/frontpage1.jpeg";
+import frontpage from "../../../assets/frontpage.jpeg";
+import front4 from "../../../assets/front4.jpeg";
+export const maincrasolData = [
+  {
+    image: frontpage, // Correct way to use local images in public folder
+  },
+  {
+    image: front4,
+  },
+  {
+    image: frontpage1,
+  }
+];
