4b777bc756d674b46b5ca0f15285dc37adde65a7
sakshigithubssuk <165806049+sakshigithubssuk@users.noreply.github.com>
2025-06-28 18:22:42 +0530

    Update server.js

--- Node-api/src/server.js
+++ Node-api/src/server.js
@@ -1,9 +1,1 @@
-const app = require("./app"); // or './index' if your Express app is defined there
-const { connectDb } = require("./config/db");
 
-const PORT = process.env.PORT || 5454;
-
-app.listen(PORT, async () => {
-  await connectDb();
-  console.log("ecommerce API listening on port:", PORT);
-});
